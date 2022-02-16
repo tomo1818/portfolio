9b2ea67feaf387d4bfbf7bc357c1151da32a2ea7
furusetomoya <tomo.fwork18@gmail.com>
2022-02-16 13:56:57 +0900

    プロダクト詳細ページを静的生成に変更

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+// import { GetServerSideProps } from 'next';
 import { client } from '../../libs/client';
 import type { Product } from '../../types/product';
 import type { Category } from '../../types/category';
@@ -28,19 +28,45 @@ export default function BlogId({ product, categories, tags }: Props) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const id = ctx.params?.id;
-  const idExceptArray = id instanceof Array ? id[0] : id;
-  const productData = await client.get({
-    endpoint: 'product',
-    contentId: idExceptArray,
-  });
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const id = ctx.params?.id;
+//   const idExceptArray = id instanceof Array ? id[0] : id;
+//   const productData = await client.get({
+//     endpoint: 'product',
+//     contentId: idExceptArray,
+//   });
+//   const categoryData = await client.get({ endpoint: 'product-category' });
+//   const tagData = await client.get({ endpoint: 'product-tag' });
+
+//   return {
+//     props: {
+//       product: productData,
+//       categories: categoryData.contents,
+//       tags: tagData.contents,
+//     },
+//   };
+// };
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'product' });
+  console.log(data.contents);
+
+  const paths = data.contents.map((content: any) => `/product/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context: any) => {
+  console.log(context);
+  const id = context.params.id;
+  const data = await client.get({ endpoint: 'product', contentId: id });
   const categoryData = await client.get({ endpoint: 'product-category' });
   const tagData = await client.get({ endpoint: 'product-tag' });
 
   return {
     props: {
-      product: productData,
+      product: data,
       categories: categoryData.contents,
       tags: tagData.contents,
     },
