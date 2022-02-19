09b0a63d673a15600221c092c87ab87deb300937
furusetomoya <tomo.fwork18@gmail.com>
2022-02-19 23:07:15 +0900

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,16 @@ import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import theme from '../theme';
+import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <div className='App'>
+        <Head>
+          <link rel='shortcut icon' href='/favicon.svg' />
+        </Head>
         <Header></Header>
         <Component {...pageProps} />
         <Footer></Footer>
