ea6374e08e7cf63c41370f62304c99587cfd2e71
furusetomoya <tomo.fwork18@gmail.com>
2022-02-26 19:56:35 +0900

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <div className='App'>
         <Head>
+          <title>ポートフォリオ</title>
           <link rel='shortcut icon' href='/favicon.svg' />
         </Head>
         <Header></Header>
